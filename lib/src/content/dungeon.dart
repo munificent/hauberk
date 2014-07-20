60058f7b7d791cbea0e8665f21069025bccf940d
Bob Nystrom <robert@stuffwithstuff.com>
2014-07-20 11:07:24 -0700

    Fix TODOs in dungeon.dart.

--- lib/src/content/dungeon.dart
+++ lib/src/content/dungeon.dart
@@ -14,8 +14,8 @@ class TrainingGrounds extends Dungeon {
     addRooms(300);
     addJunctions(30);
 
-    carveRooms();
-    carveCorridors();
+    _carveRooms();
+    _carveCorridors();
 
     // We do this after carving corridors so that when corridors carve through
     // rooms, they don't mess up the decorations.
@@ -27,7 +27,7 @@ class TrainingGrounds extends Dungeon {
     // We do this last so that we only add doors where they actually make sense
     // and don't have to worry about overlapping corridors and other stuff
     // leading to nonsensical doors.
-    addDoors();
+    _addDoors();
   }
 }
 
@@ -50,22 +50,22 @@ class GoblinStronghold extends Dungeon {
     // Layout the rooms.
     addRooms(_numRooms);
 
-    carveRooms();
-    carveCorridors();
+    _carveRooms();
+    _carveCorridors();
 
-    decorateRooms((room) {
-      if (rng.oneIn(5)) decorateTable(room);
+    _decorateRooms((room) {
+      if (rng.oneIn(5)) _decorateTable(room);
     });
 
-    addDoors();
+    _addDoors();
   }
 
-  bool allowRoomOverlap(Rect a, Rect b) => rng.oneIn(20);
+  bool _allowRoomOverlap(Rect a, Rect b) => rng.oneIn(20);
 }
 
 abstract class Dungeon extends StageBuilder {
-  final List<Room> _rooms;
-  final Set<int> _usedColors;
+  final _rooms = <_Room>[];
+  final _usedColors = new Set<int>();
 
   int get roomWidthMin => 3;
   int get roomWidthMax => 12;
@@ -84,9 +84,7 @@ abstract class Dungeon extends StageBuilder {
   TileType get floor => Tiles.floor;
   TileType get wall => Tiles.wall;
 
-  Dungeon()
-    : _rooms = <Room>[],
-      _usedColors = new Set<int>();
+  Dungeon();
 
   void generate(Stage stage) {
     bindStage(stage);
@@ -99,9 +97,9 @@ abstract class Dungeon extends StageBuilder {
   void addRooms(int tries) {
     for (var i = 0; i < tries; i++) {
       var room = randomRoom();
-      if (!overlapsExistingRooms(room, false)) {
+      if (!_overlapsExistingRooms(room, false)) {
         var color = _usedColors.length;
-        _rooms.add(new Room(room, color));
+        _rooms.add(new _Room(room, color));
         _usedColors.add(color);
       }
     }
@@ -118,23 +116,23 @@ abstract class Dungeon extends StageBuilder {
 
   void addJunctions(int tries) {
     for (var i = 0; i < tries; i++) {
-      final x = rng.range(1, stage.width - 3);
-      final y = rng.range(1, stage.height - 3);
+      var x = rng.range(1, stage.width - 3);
+      var y = rng.range(1, stage.height - 3);
 
-      final room = new Rect(x, y, 1, 1);
-      if (!overlapsExistingRooms(room, true)) {
+      var room = new Rect(x, y, 1, 1);
+      if (!_overlapsExistingRooms(room, true)) {
         var color = _usedColors.length;
-        _rooms.add(new Room(room, ++color));
+        _rooms.add(new _Room(room, ++color));
         _usedColors.add(color);
       }
     }
   }
 
-  bool overlapsExistingRooms(Rect room, bool isJunction) {
-    for (final other in _rooms) {
+  bool _overlapsExistingRooms(Rect room, bool isJunction) {
+    for (var other in _rooms) {
       if (room.distanceTo(other.bounds) <= 0) {
         // Possibly allow some rooms to overlap.
-        if (!isJunction && allowRoomOverlap(room, other.bounds)) continue;
+        if (!isJunction && _allowRoomOverlap(room, other.bounds)) continue;
         return true;
       }
     }
@@ -142,51 +140,45 @@ abstract class Dungeon extends StageBuilder {
     return false;
   }
 
-  bool allowRoomOverlap(Rect a, Rect b) => false;
+  bool _allowRoomOverlap(Rect a, Rect b) => false;
 
-  void mergeColors(Room a, Room b) {
+  void _mergeColors(_Room a, _Room b) {
     if (a.color == b.color) return;
 
-    final color = math.min(a.color, b.color);
+    var color = math.min(a.color, b.color);
     _usedColors.remove(math.max(a.color, b.color));
 
-    for (final room in _rooms) {
+    for (var room in _rooms) {
       if (room.color == a.color || room.color == b.color) {
         room.color = color;
       }
     }
   }
 
-  void carveRooms() {
+  void _carveRooms() {
     // Fill them in.
-    for (final room in _rooms) {
-      for (final pos in room.bounds) {
+    for (var room in _rooms) {
+      for (var pos in room.bounds) {
         setTile(pos, Tiles.floor);
       }
     }
 
     // Unify colors for rooms that are already overlapping.
-    // TODO(bob): Inner loop shouldn't go through all rooms. Redundantly
-    // considers each pair twice.
-    for (final room in _rooms) {
-      for (final other in _rooms) {
-        if (room == other) continue;
-        if (room.bounds.distanceTo(other.bounds) <= 0) {
-          mergeColors(room, other);
+    _forEachRoomPair((room, other) {
+      if (room.bounds.distanceTo(other.bounds) > 0) return;
+      _mergeColors(room, other);
 
-          // Keep track of which rooms overlap.
-          room.isOverlapping = true;
-          other.isOverlapping = true;
-        }
-      }
-    }
+      // Keep track of which rooms overlap.
+      room.isOverlapping = true;
+      other.isOverlapping = true;
+    });
   }
 
-  void carveCorridors() {
+  void _carveCorridors() {
     // Keep adding corridors until all rooms are connected.
     while (_usedColors.length > 1) {
       // Pick a random color.
-      final fromColor = rng.item(_rooms).color;
+      var fromColor = rng.item(_rooms).color;
 
       // Find the room that is nearest to any room of this color and has a
       // different color. (In other words, find the nearest unconnected room to
@@ -195,52 +187,44 @@ abstract class Dungeon extends StageBuilder {
       var nearestTo = null;
       var nearestDistance = 9999;
 
-      // TODO(bob): Inner loop shouldn't go through all rooms. Redundantly
-      // considers each pair twice.
-      for (final fromRoom in _rooms) {
+      for (var fromRoom in _rooms) {
         if (fromRoom.color != fromColor) continue;
 
-        for (final toRoom in _rooms) {
+        for (var toRoom in _rooms) {
           if (toRoom.color == fromColor) continue;
 
-          final distance = fromRoom.bounds.distanceTo(toRoom.bounds);
-          if (distance < nearestDistance) {
-            nearestFrom = fromRoom;
-            nearestTo = toRoom;
-            nearestDistance = distance;
-          }
+          var distance = fromRoom.bounds.distanceTo(toRoom.bounds);
+          if (distance >= nearestDistance) continue;
+          nearestFrom = fromRoom;
+          nearestTo = toRoom;
+          nearestDistance = distance;
         }
       }
 
-      carveCorridor(nearestFrom, nearestTo);
+      _carveCorridor(nearestFrom, nearestTo);
     }
 
     // Add some extra corridors so the dungeon isn't a minimum spanning tree.
-    for (final fromRoom in _rooms) {
-      for (final toRoom in _rooms) {
-        if (fromRoom == toRoom) continue;
-
-        final distance = fromRoom.bounds.distanceTo(toRoom.bounds);
-        if ((distance < extraCorridorDistanceMax) &&
-            rng.oneIn(extraCorridorOneIn + distance *
-                extraCorridorDistanceMultiplier)) {
-          carveCorridor(fromRoom, toRoom);
-        }
-      }
-    }
+    _forEachRoomPair((fromRoom, toRoom) {
+      var distance = fromRoom.bounds.distanceTo(toRoom.bounds);
+      if (distance >= extraCorridorDistanceMax) return;
+      if (!rng.oneIn(extraCorridorOneIn + distance *
+              extraCorridorDistanceMultiplier)) return;
+      _carveCorridor(fromRoom, toRoom);
+    });
   }
 
-  void carveCorridor(Room fromRoom, Room toRoom) {
-    mergeColors(fromRoom, toRoom);
+  void _carveCorridor(_Room fromRoom, _Room toRoom) {
+    _mergeColors(fromRoom, toRoom);
 
     // If the rooms overlap horizontally, carve a vertical path.
-    final left = math.max(fromRoom.bounds.left, toRoom.bounds.left);
-    final right = math.min(fromRoom.bounds.right, toRoom.bounds.right);
+    var left = math.max(fromRoom.bounds.left, toRoom.bounds.left);
+    var right = math.min(fromRoom.bounds.right, toRoom.bounds.right);
     if (left < right) {
-      final top = math.min(fromRoom.bounds.top, toRoom.bounds.top);
-      final bottom = math.max(fromRoom.bounds.bottom, toRoom.bounds.bottom);
+      var top = math.min(fromRoom.bounds.top, toRoom.bounds.top);
+      var bottom = math.max(fromRoom.bounds.bottom, toRoom.bounds.bottom);
 
-      final x = rng.range(left, right);
+      var x = rng.range(left, right);
       for (var y = top; y < bottom; y++) {
         setTile(new Vec(x, y), Tiles.floor);
       }
@@ -249,13 +233,13 @@ abstract class Dungeon extends StageBuilder {
     }
 
     // If the rooms overlap horizontally, carve a horizontal path.
-    final top = math.max(fromRoom.bounds.top, toRoom.bounds.top);
-    final bottom = math.min(fromRoom.bounds.bottom, toRoom.bounds.bottom);
+    var top = math.max(fromRoom.bounds.top, toRoom.bounds.top);
+    var bottom = math.min(fromRoom.bounds.bottom, toRoom.bounds.bottom);
     if (top < bottom) {
-      final left = math.min(fromRoom.bounds.left, toRoom.bounds.left);
-      final right = math.max(fromRoom.bounds.right, toRoom.bounds.right);
+      var left = math.min(fromRoom.bounds.left, toRoom.bounds.left);
+      var right = math.max(fromRoom.bounds.right, toRoom.bounds.right);
 
-      final y = rng.range(top, bottom);
+      var y = rng.range(top, bottom);
       for (var x = left; x < right; x++) {
         setTile(new Vec(x, y), Tiles.floor);
       }
@@ -264,10 +248,9 @@ abstract class Dungeon extends StageBuilder {
     }
 
     // We can't draw a straight corridor, so make an angled one.
-    final from = rng.vecInRect(fromRoom.bounds);
-    final to = rng.vecInRect(toRoom.bounds);
+    var from = rng.vecInRect(fromRoom.bounds);
+    var to = rng.vecInRect(toRoom.bounds);
 
-    // TODO(bob): Make corridor meander more.
     var pos = from;
     while (pos != to) {
       if (pos.y < to.y) {
@@ -284,9 +267,9 @@ abstract class Dungeon extends StageBuilder {
     }
   }
 
-  void decorateRooms(void decorateRoom(Rect room)) {
+  void _decorateRooms(void decorateRoom(Rect room)) {
     for (var i = 0; i < _rooms.length; i++) {
-      final room = _rooms[i];
+      var room = _rooms[i];
 
       // Don't decorate overlapping rooms.
       var overlap = false;
@@ -303,14 +286,14 @@ abstract class Dungeon extends StageBuilder {
   }
 
   /// Add rows of pillars to the edge(s) of the room.
-  bool decoratePillars(Rect room) {
+  bool _decoratePillars(Rect room) {
     if (room.width < 5) return false;
     if (room.height < 5) return false;
 
     // Only odd-sized sides get them, so make sure at least one side is.
     if ((room.width % 2 == 0) && (room.height % 2 == 0)) return false;
 
-    final type = rng.oneIn(2) ? Tiles.wall : Tiles.lowWall;
+    var type = rng.oneIn(2) ? Tiles.wall : Tiles.lowWall;
 
     if (room.width % 2 == 1) {
       for (var x = room.left + 1; x < room.right - 1; x += 2) {
@@ -331,18 +314,18 @@ abstract class Dungeon extends StageBuilder {
 
   /// If [room] is big enough, adds a floating room inside of it with a single
   /// entrance.
-  bool decorateInnerRoom(Rect room) {
+  bool _decorateInnerRoom(Rect room) {
     if (room.width < 5) return false;
     if (room.height < 5) return false;
 
-    final width = rng.range(3, room.width  - 2);
-    final height = rng.range(3, room.height - 2);
-    final x = rng.range(room.x + 1, room.right - width);
-    final y = rng.range(room.y + 1, room.bottom - height);
+    var width = rng.range(3, room.width  - 2);
+    var height = rng.range(3, room.height - 2);
+    var x = rng.range(room.x + 1, room.right - width);
+    var y = rng.range(room.y + 1, room.bottom - height);
 
     // Trace the room.
-    final type = rng.oneIn(2) ? Tiles.wall : Tiles.lowWall;
-    for (final pos in new Rect(x, y, width, height).trace()) {
+    var type = rng.oneIn(2) ? Tiles.wall : Tiles.lowWall;
+    for (var pos in new Rect(x, y, width, height).trace()) {
       setTile(pos, type);
     }
 
@@ -378,7 +361,7 @@ abstract class Dungeon extends StageBuilder {
   }
 
   /// Places a table in the room.
-  bool decorateTable(Rect room) {
+  bool _decorateTable(Rect room) {
     var pos = rng.vecInRect(room);
 
     // Don't block an exit.
@@ -402,57 +385,57 @@ abstract class Dungeon extends StageBuilder {
     return true;
   }
 
-  bool isFloor(int x, int y) => stage.get(x, y).type == Tiles.floor;
-  bool isWall(int x, int y) => stage.get(x, y).type == Tiles.wall;
+  bool _isFloor(int x, int y) => stage.get(x, y).type == Tiles.floor;
+  bool _isWall(int x, int y) => stage.get(x, y).type == Tiles.wall;
 
-  void addDoors() {
+  void _addDoors() {
     // For each room, attempt to place doors along its edges.
-    for (final room in _rooms) {
+    for (var room in _rooms) {
       // Top and bottom.
       for (var x = room.bounds.left; x < room.bounds.right; x++) {
-        tryHorizontalDoor(x, room.bounds.top - 1);
-        tryHorizontalDoor(x, room.bounds.bottom);
+        _tryHorizontalDoor(x, room.bounds.top - 1);
+        _tryHorizontalDoor(x, room.bounds.bottom);
       }
 
       // Left and right.
       for (var y = room.bounds.top; y < room.bounds.bottom; y++) {
-        tryVerticalDoor(room.bounds.left - 1, y);
-        tryVerticalDoor(room.bounds.right, y);
+        _tryVerticalDoor(room.bounds.left - 1, y);
+        _tryVerticalDoor(room.bounds.right, y);
       }
     }
   }
 
-  void tryHorizontalDoor(int x, int y) {
+  void _tryHorizontalDoor(int x, int y) {
     // Must be an opening where the door will be.
-    if (!isFloor(x, y)) return;
+    if (!_isFloor(x, y)) return;
 
     // Must be wall on either end of the door.
-    if (!isWall(x - 1, y)) return;
-    if (!isWall(x + 1, y)) return;
+    if (!_isWall(x - 1, y)) return;
+    if (!_isWall(x + 1, y)) return;
 
     // And open in front and behind it.
-    if (!isFloor(x, y - 1)) return;
-    if (!isFloor(x, y + 1)) return;
+    if (!_isFloor(x, y - 1)) return;
+    if (!_isFloor(x, y + 1)) return;
 
-    addDoor(x, y);
+    _addDoor(x, y);
   }
 
-  void tryVerticalDoor(int x, int y) {
+  void _tryVerticalDoor(int x, int y) {
     // Must be an opening where the door will be.
-    if (!isFloor(x, y)) return;
+    if (!_isFloor(x, y)) return;
 
     // Must be wall on either end of the door.
-    if (!isWall(x, y - 1)) return;
-    if (!isWall(x, y + 1)) return;
+    if (!_isWall(x, y - 1)) return;
+    if (!_isWall(x, y + 1)) return;
 
     // And open in front and behind it.
-    if (!isFloor(x - 1, y)) return;
-    if (!isFloor(x + 1, y)) return;
+    if (!_isFloor(x - 1, y)) return;
+    if (!_isFloor(x + 1, y)) return;
 
-    addDoor(x, y);
+    _addDoor(x, y);
   }
 
-  void addDoor(int x, int y) {
+  void _addDoor(int x, int y) {
     var type = Tiles.floor;
 
     var roll = rng.range(100);
@@ -464,12 +447,21 @@ abstract class Dungeon extends StageBuilder {
 
     setTile(new Vec(x, y), type);
   }
+
+  /// Invokes [callback] on each unique pair of [Room]s.
+  void _forEachRoomPair(callback(_Room a, _Room b)) {
+    for (var i = 0; i < _rooms.length - 1; i++) {
+      for (var j = i + 1; j < _rooms.length; j++) {
+        callback(_rooms[i], _rooms[j]);
+      }
+    }
+  }
 }
 
-class Room {
+class _Room {
   final Rect bounds;
   int color;
   bool isOverlapping = false;
 
-  Room(this.bounds, this.color);
+  _Room(this.bounds, this.color);
 }
