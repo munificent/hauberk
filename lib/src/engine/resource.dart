2567e7359f2e07ac945331d5c4f6c1415a2a3384
Bob Nystrom <robert@stuffwithstuff.com>
2017-01-07 16:42:32 -0800

    Change math and optimize random resource choice.
    The heart of room, item, affix, and monster generation is ResourceSet,
    and its methods for choosing a random object at a given depth. Its goal
    is to prefer selecting an object near that depth, with some sort of
    bell curve-like fall off on either side. It used an accumulation of
    weird hacks and goofy math to approximate that. It was also slow.
    
    Now, each resource has an actual normal distribution centered on its
    level. The left and right sides of the bell curve are carefully tweaked
    to, I hope give nice odds of choosing out of depth (above and below)
    stuff, taking into account the depth the resource is being chosen for.
    
    Doing this "properly" involves a good bit of math and creating a
    probability table of all of the resources. That's slow. Fortunately, we
    can cache that and reuse it across calls.
    
    With that caching in place, and a binary search over the weighted
    table, this now selects resources about 20x faster than the old code.

--- lib/src/engine/resource.dart
+++ lib/src/engine/resource.dart
@@ -1,8 +1,13 @@
+import 'dart:math' as math;
+
 import 'package:piecemeal/piecemeal.dart';
 
 class ResourceSet<T> {
   final Map<String, _Tag<T>> _tags = {};
   final Map<String, _Resource<T>> _resources = {};
+
+  // TODO: Evict old queries from the cache if it gets too large.
+  final Map<_QueryKey, _ResourceQuery<T>> _queries = {};
 
   bool get isEmpty => _resources.isEmpty;
   bool get isNotEmpty => _resources.isNotEmpty;
@@ -68,11 +73,15 @@ class ResourceSet<T> {
 
   /// Chooses a random resource in [tagName] for [depth].
   ///
-  /// Includes all resources of child tags of [tagName]. There is also a random
-  /// chance that this will walk "up" and include resources from a parent tag
-  /// of [tagName].
+  /// Includes all resources of child tags of [tagName]. For example, given tag
+  /// path "equipment/weapon/sword", if [tagName] is "weapon", this will permit
+  /// resources tagged "weapon" or "sword", but not "equipment" or other child
+  /// tags of "equipment" (unless the random chance to walk up to it succeeded).
   ///
-  /// May return `null` if there are no tagged items close enough to the depth.
+  /// There is also a random chance that this will walk "up" and include
+  /// resources from a parent tag of [tagName].
+  ///
+  /// May return `null` if there are no resources with [tagName].
   T tryChoose(int depth, String tagName) {
     assert(tagName != null);
     var tag = _tags[tagName];
@@ -83,7 +92,7 @@ class ResourceSet<T> {
       tag = tag.parent;
     }
 
-    return _tryChoose(depth, (resource) {
+    return _runQuery(tag.name, depth, (resource) {
       for (var resourceTag in resource._tags) {
         if (resourceTag.contains(tag)) return true;
       }
@@ -105,7 +114,10 @@ class ResourceSet<T> {
       return tag;
     });
 
-    return _tryChoose(depth, (resource) {
+    var tagNames = tags.toList();
+    tagNames.sort();
+
+    return _runQuery("${tagNames.join('|')} (match)", depth, (resource) {
       for (var resourceTag in resource._tags) {
         if (tagObjects.any((tag) => tag.contains(resourceTag))) return true;
       }
@@ -114,61 +126,64 @@ class ResourceSet<T> {
     });
   }
 
-  T _tryChoose(int depth, bool predicate(_Resource<T> resource)) {
-    var minDepth = depth ~/ 2;
+  T _runQuery(String name, int depth, bool predicate(_Resource<T> resource)) {
+    // Reuse a cached query, if possible.
+    var key = new _QueryKey(name, depth);
+    var query = _queries[key];
+    if (query == null) {
+      var allowed = _resources.values.where(predicate).toList(growable: false);
 
-    // Bell curve around goal depth.
-    var width = depth ~/ 5;
-    for (var i = 0; i < 3; i++) {
-      depth += rng.triangleInt(0, width);
+      // Determine the weighted chance for each resource.
+      var chances = new List<double>.filled(allowed.length, 0.0, growable: false);
+      var chance = 0.0;
+      for (var i = 0; i < allowed.length; i++) {
+        var resource = allowed[i];
+        chance += resource.frequency * _depthScale(depth, resource.depth);
+        chances[i] = chance;
+      }
+
+      query = new _ResourceQuery<T>(depth, allowed, chances, chance);
+      _queries[key] = query;
     }
 
-    // Chance of out-of-depth items.
-    if (rng.oneIn(1000)) {
-      depth += rng.range(25, 100);
-    } else if (rng.oneIn(100)) {
-      depth += rng.range(5, 25);
-    } else if (rng.oneIn(10)) {
-      depth += rng.range(1, 5);
-    }
-
-    // Find all of the objects at or below the max depth and with the tag.
-    var allowed = _resources.values
-        .where((resource) => resource.depth >= minDepth &&
-        resource.depth <= depth &&
-        predicate(resource));
-
-    if (allowed.isEmpty) return null;
-
-    // Determine the weighted range to choose from.
-    var totalFrequency = allowed.fold(0.0,
-        (frequency, resource) => frequency += resource.frequency);
-
-    // Pick an item. Try a few times and take the best.
-    var resource = _pickWeighted(allowed, totalFrequency);
-    // TODO: Make tunable?
-    for (var i = 0; i < 1; i++) {
-      var other = _pickWeighted(allowed, totalFrequency);
-      if (other.depth > resource.depth) resource = other;
-    }
-
-    return resource.object;
+    return query.choose();
   }
 
-  _Resource<T> _pickWeighted(Iterable<_Resource<T>> resources,
-      double totalFrequency) {
-    // Pick a point in the probability range.
-    var t = rng.float(totalFrequency);
+  /// Gets the probability adjustment for choosing a resource with [depth] at
+  /// a goal of [targetDepth].
+  ///
+  /// This is based on a normal distribution, with some tweaks. Unlike the
+  /// real normal distribution, this does *not* ensure that all probabilities
+  /// sum to zero. We don't need to since we normalize separately.
+  ///
+  /// Instead, this always returns `1.0` for the most probable [depth], which is
+  /// when it's equal to [targetDepth]. On either side of that, we have a bell
+  /// curve. The curve widens as you go deeper in the dungeon. This reflects
+  /// the fact that encountering a depth 4 monster at depth 1 is a lot more
+  /// dangerous than a depth 54 monster at depth 51.
+  ///
+  /// The curve is also asymmetric. It widens out more quickly on the left.
+  /// This means that as you venture deeper, weaker things you've already seen
+  /// "linger" and are more likely to appear than out-of-depth *stronger*
+  /// things are.
+  ///
+  /// https://en.wikipedia.org/wiki/Normal_distribution
+  double _depthScale(int depth, int targetDepth) {
+    var distance = (depth - targetDepth).toDouble();
+    double deviation;
+    if (distance <= 0.0) {
+      // As you get deeper in the dungeon, the probability curve widens so that
+      // you still find weaker stuff fairly frequently.
+      deviation = 0.2 + targetDepth * 0.3;
 
-    // Find it in the weighted list.
-    // TODO: Use binary search instead of linear.
-    for (var resource in resources) {
-      if (t < resource.frequency) return resource;
-
-      t -= resource.frequency;
+      // But don't let it get *too* wide. We don't want the hero finding a lot
+      // of sticks at the bottom of the dungeon.
+      if (deviation > 20.0) deviation = 20.0;
+    } else {
+      deviation = 0.4 + targetDepth * 0.1;
     }
 
-    throw "Unreachable.";
+    return math.exp(-0.5 * distance * distance / (deviation * deviation));
   }
 }
 
@@ -196,5 +211,68 @@ class _Tag<T> {
     }
 
     return false;
+  }
+}
+
+/// Uniquely identifies a query.
+class _QueryKey {
+  final String name;
+  final int depth;
+
+  _QueryKey(this.name, this.depth);
+
+  int get hashCode => name.hashCode ^ depth.hashCode;
+  bool operator ==(other) {
+    assert(other is _QueryKey);
+    return name == other.name && depth == other.depth;
+  }
+}
+
+/// A stored query that let us quickly choose a random weighted resource for
+/// some given criteria.
+///
+/// The basic process for picking a random resource is:
+///
+/// 1. Find all of the resources that could be chosen.
+/// 2. Calculate the chance of choosing each item.
+/// 3. Pick a random number up to the total chance.
+/// 4. Find the resource whose chance contains that number.
+///
+/// The first two steps are quite slow: they involve iterating over all
+/// resources, allocating a list, etc. Fortunately, we can reuse the results of
+/// them for every call to [tryChoose] or [tryChooseMatching] with the same
+/// arguments.
+///
+/// This caches that state.
+class _ResourceQuery<T> {
+  final int depth;
+  final List<_Resource<T>> resources;
+  final List<double> chances;
+  final double totalChance;
+
+  _ResourceQuery(this.depth, this.resources, this.chances, this.totalChance);
+
+  /// Choose a random resource that matches this query.
+  T choose() {
+    if (resources.isEmpty) return null;
+
+    // Pick a point in the probability range.
+    var t = rng.float(totalChance);
+
+    // Binary search to find the resource in that chance range.
+    var first = 0;
+    var last = resources.length - 1;
+
+    while (true) {
+      var middle = (first + last) ~/ 2;
+
+      if (middle > 0 && t < chances[middle - 1]) {
+        last = middle - 1;
+      } else if (t < chances[middle]) {
+        return resources[middle].object;
+      } else {
+        first = middle + 1;
+      }
+    }
   }
 }
