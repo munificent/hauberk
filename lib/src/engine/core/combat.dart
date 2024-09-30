1c9dd87bccc3160900f64613da0c257f832c97cf
Robert Nystrom <rnystrom@google.com>
2024-09-29 18:15:19 -0700

    Don't show damage number in messages.

--- lib/src/engine/core/combat.dart
+++ lib/src/engine/core/combat.dart
@@ -191,8 +191,7 @@ class Hit {
 
     action.addEvent(EventType.hit,
         actor: defender, element: element, other: damage);
-    action.log(
-        '{1} ${_attack.verb} {2} for $damage damage.', attackNoun, defender);
+    action.log('{1} ${_attack.verb} {2}.', attackNoun, defender);
     return damage;
   }
 
