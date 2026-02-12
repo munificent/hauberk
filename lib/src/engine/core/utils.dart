52f763e4a98e83ee37ac0ee2ad7314d98bb2af35
Robert Nystrom <robert@stuffwithstuff.com>
2026-02-12 07:36:40 -0800

    Support negative numbers and leading "+" in int.fmt().

--- lib/src/engine/core/utils.dart
+++ lib/src/engine/core/utils.dart
@@ -1,30 +1,30 @@
 extension IntExtensions on int {
+  static final RegExp _groups = RegExp(r"^(\d{1,3})((\d{3})+)$");
+
   /// Converts the number to a string, with separating commas for thousands,
   /// millions, etc.
   ///
   /// If [w] is given, pads the result on the left to at least that many
   /// characters.
-  String fmt({int? w}) {
-    var result = toString();
+  String fmt({int? w, bool sign = false}) {
+    var result = abs().toString();
 
-    // TODO: Handle negative numbers.
+    // Separate digit groups with commas.
+    if (_groups.firstMatch(result) case var match?) {
+      var rest = match[2]!;
+      var sections = [
+        match[1]!,
+        for (var i = 0; i < rest.length ~/ 3; i++)
+          rest.substring(i * 3, i * 3 + 3),
+      ];
 
-    if (this > 999999999) {
-      result =
-          "${result.substring(0, result.length - 9)},"
-          "${result.substring(result.length - 9)}";
+      result = sections.join(",");
     }
 
-    if (this > 999999) {
-      result =
-          "${result.substring(0, result.length - 6)},"
-          "${result.substring(result.length - 6)}";
-    }
-
-    if (this > 999) {
-      result =
-          "${result.substring(0, result.length - 3)},"
-          "${result.substring(result.length - 3)}";
+    if (this < 0) {
+      result = "-$result";
+    } else if (this > 0 && sign) {
+      result = "+$result";
     }
 
     return result.padLeft(w ?? 0);
